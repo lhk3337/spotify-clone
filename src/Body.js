7a5157f5b322ebf5acd97ed0e80e7427ff192d6a
lhk3337 <lhk3337@naver.com>
2020-08-26 23:03:38 +0900

    build footer  and left sidebar

---
+++ src/Body.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./style/Body.css";
+function Body() {
+  return (
+    <div className="body">
+      <h1>hello body</h1>
+    </div>
+  );
+}
+
+export default Body;
--- src/Player.js
+++ src/Player.js
@@ -1,9 +1,17 @@
 import React from "react";
-
-function Player() {
+import "./style/Player.css";
+import Sidebar from "./Sidebar";
+import Body from "./Body";
+import Footer from "./Footer";
+function Player({ spotify }) {
   return (
-    <div>
-      <h1>Welcome to spotify</h1>
+    <div className="player">
+      <div className="player__body">
+        <Sidebar />
+        <Body />
+      </div>
+
+      <Footer />
     </div>
   );
 }
